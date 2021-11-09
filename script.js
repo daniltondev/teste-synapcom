a83ec81b1ad407aafd5ae1e8a3b66ff819b37fde
daniltondev <52671086+daniltondev@users.noreply.github.com>
2021-11-09 17:15:24 -0300

    commit inicial
    Site em desenvolvimento como teste

---
+++ script.js
@@ -0,0 +1,28 @@
+/* SLICK FUNCTIONS */
+
+$('.banner-carrossel').slick({
+  dots: true,
+  infinite: true,
+  arrows: true,
+  speed: 1000,
+  autoplay: true,
+  autoplaySpeed: 3000
+})
+
+$('.smarthint-news-content').slick({
+  dots: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000
+})
+
+$('.smarthint-bestsellers-content').slick({
+  dots: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000
+})
+
+console.log('tá aqui')
