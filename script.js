0e7ac8a4fa6bacbae266deec2c82db300760f536
daniltondev <52671086+daniltondev@users.noreply.github.com>
2021-11-17 17:52:24 -0300

    Feito Menu
    Menu Drop criado

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ $('.smarthint-news-content').slick({
   dots: true,
   slidesToShow: 4,
   slidesToScroll: 1,
-  autoplay: true,
+  autoplay: false,
   autoplaySpeed: 2000
 })
 
@@ -21,8 +21,78 @@ $('.smarthint-bestsellers-content').slick({
   dots: true,
   slidesToShow: 4,
   slidesToScroll: 1,
-  autoplay: true,
+  autoplay: false,
   autoplaySpeed: 2000
 })
 
 console.log('tá aqui')
+
+//MENU
+
+$('.arrow-novidades').hover(
+  function () {
+    $('#novidades').css({ display: 'unset', 'margin-top': '45px' })
+  },
+  function () {
+    $('#novidades').css('display', 'none')
+  }
+)
+
+$('.arrow-meninas').hover(
+  function () {
+    $('#meninas').css({ display: 'unset', 'margin-top': '45px' })
+  },
+  function () {
+    $('#meninas').css('display', 'none')
+  }
+)
+
+$('.arrow-meninos').hover(
+  function () {
+    $('#meninos').css({ display: 'unset', 'margin-top': '45px' })
+  },
+  function () {
+    $('#meninos').css('display', 'none')
+  }
+)
+
+$('.arrow-bebes').hover(
+  function () {
+    $('#bebes').css({ display: 'unset', 'margin-top': '45px' })
+  },
+  function () {
+    $('#bebes').css('display', 'none')
+  }
+)
+
+$('.arrow-marcas').hover(
+  function () {
+    $('#marcas').css({ display: 'unset', 'margin-top': '45px' })
+  },
+  function () {
+    $('#marcas').css('display', 'none')
+  }
+)
+
+$('.arrow-promocoes').hover(
+  function () {
+    $('#promocoes').css({ display: 'unset', 'margin-top': '45px' })
+  },
+  function () {
+    $('#promocoes').css('display', 'none')
+  }
+)
+
+//SWEET ALERT
+
+let localStorageContent = localStorage.getItem('lgpd-confirm')
+if (!localStorageContent) {
+  $('.lgpd-sweet-alert').css('display', 'unset')
+
+  let lgpdArea = $('.lgpd-sweet-alert')
+  let lgpdBtn = $('.lgpd-item-button')
+  lgpdBtn.on('click', function () {
+    lgpdArea.remove()
+    localStorage.setItem('lgpd-confirm', 'OK')
+  })
+}
